c8a7568fb77708b7a81bbb0c130e2c6ef34dc5ac
bielb1b2 <gabrielvt.souza@hotmail.com>
2024-10-25 16:26:17 -0300

    feat: add resizable screen

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gcc -o window window.c `pkg-config --libs raylib`
+gcc -o window window.c `pkg-config --libs raylib` -lm
--- window.c
+++ window.c
@@ -22,10 +22,19 @@
 ********************************************************************************************/
 
 #include "raylib.h"
+#include "math.h"
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+
+float CalculateAngle(Vector2 start, Vector2 end)
+{
+    float deltaX = end.x - start.x;
+    float deltaY = end.y - start.y;
+    return atan2f(deltaY, deltaX);  // Função integrada para calcular ângulo
+}
+
 int main(void)
 {
     // Initialization
@@ -35,7 +44,7 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    SetTargetFPS(90);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
 
@@ -50,24 +59,37 @@ int main(void)
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-            ClearBackground(RAYWHITE);
+        int currentScreenHeight = GetScreenHeight();
+        int currentScreenWidth = GetScreenWidth();
 
-            // Draw x1 axis
-            DrawLine(0, screenHeight/2, screenWidth, screenHeight/2, BLACK);
-            // Draw y1 axis
-            DrawLine(screenWidth/2, 0, screenWidth/2, screenHeight, BLACK);
+        Vector2 mousePosition = GetMousePosition();
+        Vector2 center = { currentScreenWidth / 2.0f, currentScreenHeight / 2.0f};
 
-            // Draw x2 axis
-            DrawLine(0, 0, screenWidth, screenHeight, RED);
-            // Draw y2 axis
-            DrawLine(0, screenHeight, screenWidth, 0, RED);
+        float angle = CalculateAngle(center, mousePosition);
+        float axisLength = (currentScreenWidth < currentScreenHeight ? currentScreenWidth : currentScreenHeight);
 
-            
+        Vector2 xAxisEnd = {
+            center.x + cos(angle) * axisLength,
+            center.y + sin(angle) * axisLength
+        };
 
-            DrawGrid(5, 10);
+        Vector2 yAxisEnd = {
+            center.x - sin(angle) * axisLength,
+            center.y + cos(angle) * axisLength
+        };
+
+        ClearBackground(RAYWHITE);
+
+        // Draw x1 axis
+        DrawLine(0, currentScreenHeight/2, currentScreenWidth, currentScreenHeight/2, BLACK);
+        // Draw y1 axis
+        DrawLine(currentScreenWidth/2, 0, currentScreenWidth/2, currentScreenHeight, BLACK);
 
 
-            // DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        // Draw x1 axis
+        DrawLineV(center, xAxisEnd, RED);
+        DrawLineV(center, yAxisEnd, RED);
+        // DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
