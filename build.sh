b1061af4dc3278e18825ed492716bc28673819e5
burningbuffer <rodrigues3442@hotmail.com>
2024-11-29 21:23:54 -0300

    fix: fixed build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ gcc -c clear_button.c -o clear_button.o -g3 -std=c99 -Wall -Wextra -Wpedantic -O
 gcc -c utils.c -o utils.o -g3 -std=c99 -Wall -Wextra -Wpedantic -O1
 gcc -c coordinate.c -o coordinate.o -g3 -std=c99 -Wall -Wextra -Wpedantic -O1
 gcc -c main.c -o main.o -g3 -std=c99 -Wall -Wextra -Wpedantic -O1 `pkg-config --libs raylib` -lm
-gcc main.o coordinate.o utils.o clear_button.o -o relativity -g3 -std=c99 -Wall -Wextra -Wpedantic -O1 `pkg-config --libs raylib` -lm
+gcc main.o aside.o clear_button.o utils.o coordinate.o -o relativity -g3 -std=c99 -Wall -Wextra -Wpedantic -O1 `pkg-config --libs raylib` -lm
